49851d14744b4023fb55fe348af4aca19239c6bb
Calvin Cha <calvin@versusnow.io>
2021-11-09 16:19:41 +0900

    upload with container name and style up

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,164 @@
 // ./src/App.tsx
 
-import React, { useState } from 'react';
-import Path from 'path';
-import uploadFileToBlob, { isStorageConfigured } from './azure-storage-blob';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Path from "path";
+import uploadFileToBlob, {
+  isStorageConfigured,
+  getBlobsInContainer2,
+} from "./azure-storage-blob";
+
+const Main = styled("div")`
+  font-size: 1em;
+  border: 1px solid #e5e5e5;
+  display: flex;
+`;
+
+const DropDownContainer = styled("div")`
+  padding: 1px 20px;
+  width: 200px;
+`;
+
+const Button = styled("button")`
+  font-size: 1em;
+  margin-left: 20px;
+  padding: 5px 20px;
+  font-weight: 500;
+  border: none;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+  background: #ffffff;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+`;
+
+const InputLabel = styled("label")`
+  padding: 5px 10px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+  background: #ffffff;
+`;
+
+const DropDownHeader = styled("div")`
+  padding: 5px 20px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.15);
+  background: #ffffff;
+`;
+
+const DropDownList = styled("ul")`
+  padding: 8px 0px;
+  width: 200px;
+  position: absolute;
+  background-color: #f9f9f9;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
+  z-index: 1;
+  margin: 0;
+  background: #ffffff;
+  border: 2px solid #e5e5e5;
+  box-sizing: border-box;
+  &:first-child {
+    padding-top: 0.8em;
+  }
+`;
+
+const ListItem = styled("li")`
+  padding: 0px 20px;
+  list-style: none;
+  z-index: 1;
+  margin-bottom: 0.8em;
+`;
 
 const storageConfigured = isStorageConfigured();
+const options = [
+  "user-profiles",
+  "small-tile-images",
+  "big-tile-images",
+  "preview-images",
+];
 
 const App = (): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<string>();
+  const toggling = () => setIsOpen(!isOpen);
+
+  const onOptionClicked = (value: string) => () => {
+    setSelectedOption(value);
+    setIsOpen(false);
+    console.log(selectedOption);
+  };
+
   // all blobs in container
   const [blobList, setBlobList] = useState<string[]>([]);
 
   // current file to upload into container
-  const [fileSelected, setFileSelected] = useState(null);
+  const [filesSelected, setFilesSelected] = useState<FileList>();
+  //const [filesSelected, setFilesSelected] = useState([]);
 
   // UI/form management
   const [uploading, setUploading] = useState(false);
   const [inputKey, setInputKey] = useState(Math.random().toString(36));
 
   const onFileChange = (event: any) => {
-    // capture file into state
-    setFileSelected(event.target.files[0]);
+    setFilesSelected(event.target.files);
   };
 
   const onFileUpload = async () => {
     // prepare UI
     setUploading(true);
 
-    // *** UPLOAD TO AZURE STORAGE ***
-    const blobsInContainer: string[] = await uploadFileToBlob(fileSelected);
+    let files: File[] = [];
+    Array.from(filesSelected as FileList).forEach((file) => files.push(file));
 
-    // prepare UI for results
-    setBlobList(blobsInContainer);
+    let promises = files.map(async (file) => {
+      await uploadFileToBlob(
+        file,
+        selectedOption ? selectedOption : options[0]
+      );
+    });
+    await Promise.all(promises);
 
-    // reset state/form
-    setFileSelected(null);
+    setBlobList(
+      await getBlobsInContainer2(selectedOption ? selectedOption : options[0])
+    );
+
     setUploading(false);
     setInputKey(Math.random().toString(36));
   };
 
   // display form
   const DisplayForm = () => (
-    <div>
-      <input type="file" onChange={onFileChange} key={inputKey || ''} />
-      <button type="submit" onClick={onFileUpload}>
+    <Main>
+      <DropDownContainer>
+        <DropDownHeader onClick={toggling}>
+          {selectedOption || options[0]}
+        </DropDownHeader>
+        {isOpen && (
+          <DropDownList>
+            {options.map((option) => (
+              <ListItem onClick={onOptionClicked(option)} key={Math.random()}>
+                {option}
+              </ListItem>
+            ))}
+          </DropDownList>
+        )}
+      </DropDownContainer>
+      <input
+        type="file"
+        multiple
+        id="upload"
+        hidden
+        onChange={onFileChange}
+        key={inputKey || ""}
+      />
+      <InputLabel htmlFor="upload">Choose file</InputLabel>
+      <Button type="submit" onClick={onFileUpload}>
         Upload!
-          </button>
-    </div>
-  )
+      </Button>
+    </Main>
+  );
 
   // display file name and image
   const DisplayImagesFromContainer = () => (
-    <div>
+    <Main>
       <h2>Container items</h2>
       <ul>
         {blobList.map((item) => {
@@ -65,12 +173,12 @@ const App = (): JSX.Element => {
           );
         })}
       </ul>
-    </div>
+    </Main>
   );
 
   return (
     <div>
-      <h1>Upload file to Azure Blob Storage</h1>
+      <h1>Versus Data Uploader</h1>
       {storageConfigured && !uploading && DisplayForm()}
       {storageConfigured && uploading && <div>Uploading</div>}
       <hr />
@@ -81,5 +189,3 @@ const App = (): JSX.Element => {
 };
 
 export default App;
-
-
--- src/azure-storage-blob.ts
+++ src/azure-storage-blob.ts
@@ -2,23 +2,25 @@
 
 // <snippet_package>
 // THIS IS SAMPLE CODE ONLY - NOT MEANT FOR PRODUCTION USE
-import { BlobServiceClient, ContainerClient} from '@azure/storage-blob';
+import { BlobServiceClient, ContainerClient } from "@azure/storage-blob";
 
-const containerName = `tutorial-container`;
 const sasToken = process.env.REACT_APP_STORAGESASTOKEN;
-const storageAccountName = process.env.REACT_APP_STORAGERESOURCENAME; 
+const storageAccountName = process.env.REACT_APP_STORAGERESOURCENAME;
 // </snippet_package>
 
 // <snippet_isStorageConfigured>
 // Feature flag - disable storage feature to app if not configured
 export const isStorageConfigured = () => {
-  return (!storageAccountName || !sasToken) ? false : true;
-}
+  return !storageAccountName || !sasToken ? false : true;
+};
 // </snippet_isStorageConfigured>
 
 // <snippet_getBlobsInContainer>
 // return list of blobs in container to display
-const getBlobsInContainer = async (containerClient: ContainerClient) => {
+const getBlobsInContainer = async (
+  containerClient: ContainerClient,
+  containerName: string
+) => {
   const returnedBlobUrls: string[] = [];
 
   // get list of blobs in container
@@ -31,12 +33,14 @@ const getBlobsInContainer = async (containerClient: ContainerClient) => {
   }
 
   return returnedBlobUrls;
-}
+};
 // </snippet_getBlobsInContainer>
 
 // <snippet_createBlobInContainer>
-const createBlobInContainer = async (containerClient: ContainerClient, file: File) => {
-  
+const createBlobInContainer = async (
+  containerClient: ContainerClient,
+  file: File
+) => {
   // create blobClient for container
   const blobClient = containerClient.getBlockBlobClient(file.name);
 
@@ -45,11 +49,14 @@ const createBlobInContainer = async (containerClient: ContainerClient, file: Fil
 
   // upload file
   await blobClient.uploadData(file, options);
-}
+};
 // </snippet_createBlobInContainer>
 
 // <snippet_uploadFileToBlob>
-const uploadFileToBlob = async (file: File | null): Promise<string[]> => {
+const uploadFileToBlob = async (
+  file: File | null,
+  containerName: string
+): Promise<string[]> => {
   if (!file) return [];
 
   // get BlobService = notice `?` is pulled out of sasToken - if created in Azure portal
@@ -58,18 +65,39 @@ const uploadFileToBlob = async (file: File | null): Promise<string[]> => {
   );
 
   // get Container - full public read access
-  const containerClient: ContainerClient = blobService.getContainerClient(containerName);
+  const containerClient: ContainerClient =
+    blobService.getContainerClient(containerName);
   await containerClient.createIfNotExists({
-    access: 'container',
+    access: "container",
   });
 
   // upload file
   await createBlobInContainer(containerClient, file);
 
   // get list of blobs in container
-  return getBlobsInContainer(containerClient);
+  return getBlobsInContainer(containerClient, containerName);
 };
 // </snippet_uploadFileToBlob>
 
-export default uploadFileToBlob;
+export const getBlobsInContainer2 = async (containerName: string) => {
+  const returnedBlobUrls: string[] = [];
 
+  const blobService = new BlobServiceClient(
+    `https://${storageAccountName}.blob.core.windows.net/?${sasToken}`
+  );
+  const containerClient: ContainerClient =
+    blobService.getContainerClient(containerName);
+
+  // get list of blobs in container
+  // eslint-disable-next-line
+  for await (const blob of containerClient.listBlobsFlat()) {
+    // if image is public, just construct URL
+    returnedBlobUrls.push(
+      `https://${storageAccountName}.blob.core.windows.net/${containerName}/${blob.name}`
+    );
+  }
+
+  return returnedBlobUrls;
+};
+
+export default uploadFileToBlob;
